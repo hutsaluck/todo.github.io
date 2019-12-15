717146fd2d4d462112152ebebca386eb739f2284
hutsaluck <41787026+hutsaluck@users.noreply.github.com>
2019-12-15 23:15:18 +0200

    Додаю всі файли

---
+++ precache-manifest.98a9e15660f719ddbfd608b0aaf9b9c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdde1db4dd65d8f9cc3765aba4d88ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d287c2db18b0ff7d63",
+    "url": "/static/css/main.de55f840.chunk.css"
+  },
+  {
+    "revision": "1026e663629320b841f0",
+    "url": "/static/js/2.6c964e58.chunk.js"
+  },
+  {
+    "revision": "05d287c2db18b0ff7d63",
+    "url": "/static/js/main.84e85183.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
